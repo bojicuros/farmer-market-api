98316c35423d34927299cf56fea41e40c296f9df
Uros <bojicuros3@gmail.com>
2023-11-02 17:48:24 +0100

    Modified after schema update, removed lat and long attributes

--- src/validation/market.schema.ts
+++ src/validation/market.schema.ts
@@ -10,14 +10,6 @@ export const marketCreateSchema = object({
   body: object({
     name: string().required(),
     address: string().required(),
-    lat: string().matches(
-      /^-?([1-8]?[0-9]\.{1}\d{1,6}|90\.{1}0{1,6})$/,
-      "Invalid latitude value"
-    ),
-    lng: string().matches(
-      /^-?((1[0-7][0-9]|[1-9]?[0-9])\.{1}\d{1,6}|180\.{1}0{1,6})$/,
-      "Invalid longitude value"
-    ),
     image_url: string().required(),
   }),
 });
@@ -27,14 +19,6 @@ export const marketUpdateSchema = object({
     id: string().uuid().required(),
     name: string(),
     address: string(),
-    lat: string().matches(
-      /^-?([1-8]?[0-9]\.{1}\d{1,6}|90\.{1}0{1,6})$/,
-      "Invalid latitude value"
-    ),
-    lng: string().matches(
-      /^-?((1[0-7][0-9]|[1-9]?[0-9])\.{1}\d{1,6}|180\.{1}0{1,6})$/,
-      "Invalid longitude value"
-    ),
     image_url: string(),
   }),
 });

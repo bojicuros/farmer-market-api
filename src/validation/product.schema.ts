a0d3af1279ab501b23b26370272efff8a0a16b26
Uros <bojicuros3@gmail.com>
2023-11-02 17:55:33 +0100

    Updated product endpoints by adding user products endpoints for working with UserMarketProduct datatable, removed endpoints related to prices

--- src/validation/product.schema.ts
+++ src/validation/product.schema.ts
@@ -1,4 +1,4 @@
-import { InferType, number, object, string } from "yup";
+import { InferType, object, string, array } from "yup";
 
 export const productIdSchema = object({
   query: object({
@@ -9,7 +9,6 @@ export const productIdSchema = object({
 export const addProductSchema = object({
   body: object({
     name: string().required(),
-    description: string(),
     unit_of_measurement: string().required(),
   }),
 });
@@ -18,28 +17,37 @@ export const updateProductSchema = object({
   body: object({
     id: string().uuid().required(),
     name: string(),
-    description: string(),
     unit_of_measurement: string(),
   }),
 });
 
-export const productPriceAddSchema = object({
-  body: object({
-    id: string().uuid().required(),
+export const userIdSchema = object({
+  query: object({
     user_id: string().uuid().required(),
-    price_value: number().positive().required(),
   }),
 });
 
-export const marketIdSchema = object({
-  query: object({
-    market_id: string().uuid().required(),
+export const userProductAddSchema = object({
+  body: object({
+    user_id: string().required().uuid(),
+    market_id: string().required().uuid(),
+    product_ids: array().of(string().uuid()).min(1).required(),
   }),
 });
+
+export const userProductDeleteSchema = object({
+  body: object({
+    user_id: string().required().uuid(),
+    market_id: string().required().uuid(),
+    product_ids: array().of(string().uuid()).min(1).required(),
+  }),
+});
+
 export type ProductIdDto = InferType<typeof productIdSchema>["query"];
 export type AddProductDto = InferType<typeof addProductSchema>["body"];
 export type UpdateProductDto = InferType<typeof updateProductSchema>["body"];
-export type ProductPriceAddDto = InferType<
-  typeof productPriceAddSchema
+export type UserIdDto = InferType<typeof userIdSchema>["query"];
+export type UserProductAddDto = InferType<typeof userProductAddSchema>["body"];
+export type UserProductDeleteDto = InferType<
+  typeof userProductDeleteSchema
 >["body"];
-export type MarketIdDto = InferType<typeof marketIdSchema>["query"];

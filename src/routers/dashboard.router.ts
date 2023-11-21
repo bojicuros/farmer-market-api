be7f5ebb051f306baa5fce1279003b085659e230
Uros <bojicuros3@gmail.com>
2023-11-21 20:19:56 +0100

    Added authorization with appropriate roles to endpoints

--- src/routers/dashboard.router.ts
+++ src/routers/dashboard.router.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import { getDashboardInfo } from "../controllers/dashboard.controller";
+import { authorize } from "../middleware/authorization.middleware";
+import { UserRole } from "../utils/enums";
 
 const router = express.Router();
 
-router.get("/", getDashboardInfo);
+router.get("/", authorize([UserRole.Admin]), getDashboardInfo);
 
 const dashboardRouter = router;
 export default dashboardRouter;
--- src/routers/market.router.ts
+++ src/routers/market.router.ts
@@ -13,16 +13,34 @@ import {
   marketIdSchema,
   marketUpdateSchema,
 } from "../validation/market.schema";
+import { authorize } from "../middleware/authorization.middleware";
+import { UserRole } from "../utils/enums";
 
 const router = express.Router();
 
-router.get("/get-all", getAllMarkets);
 router.get("/get-all-open", getAllOpenMarkets);
-router.get("/get-by-id", validate(marketIdSchema), getMarketById);
-router.post("/create", validate(marketCreateSchema), createMarket);
-router.put("/update-by-id", validate(marketUpdateSchema), updateMarket);
+router.get("/get-all", authorize([UserRole.Admin]), getAllMarkets);
+router.get(
+  "/get-by-id",
+  authorize([UserRole.Admin]),
+  validate(marketIdSchema),
+  getMarketById
+);
+router.post(
+  "/create",
+  authorize([UserRole.Admin]),
+  validate(marketCreateSchema),
+  createMarket
+);
+router.put(
+  "/update-by-id",
+  authorize([UserRole.Admin]),
+  validate(marketUpdateSchema),
+  updateMarket
+);
 router.put(
   "/toggle-open-status",
+  authorize([UserRole.Admin]),
   validate(marketIdSchema),
   toggleMarketOpenStatus
 );

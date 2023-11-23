23be7776b0a64894b8e3607e36f4d77a13047d48
Uros <bojicuros3@gmail.com>
2023-11-23 11:51:06 +0100

    Updated search service so ordering is performed on union of result data

--- src/services/search.service.ts
+++ src/services/search.service.ts
@@ -2,13 +2,13 @@ import { prisma } from "../../src/utils/db.server";
 
 export async function search(query: string) {
   const results = await prisma.$queryRaw`
-        SELECT
+    SELECT * FROM (
+      SELECT
         'Market' AS type,
         m.name AS market_name,
         NULL AS product_name
       FROM public."Market" m
       WHERE m.name ILIKE '%' || ${query} || '%'
-      ORDER BY m.name
 
       UNION ALL
 
@@ -20,7 +20,9 @@ export async function search(query: string) {
       JOIN public."Product" p ON pph.product_id = p.id
       JOIN public."Market" m ON pph.market_id = m.id
       WHERE p.name ILIKE '%' || ${query} || '%'
-      GROUP BY m.name, p."name" 
+      GROUP BY m.name, p."name"
+    ) AS combinedResult
+    ORDER BY market_name, product_name;
   `;
   return results;
 }
